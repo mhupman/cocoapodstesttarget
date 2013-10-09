28b6230b000b6cbcc1e6621184fa605c8766c6b9
mhupman <mhupman@citrrus.com>
2013-10-09 18:54:49 -0400

    Fresh project commit

---
+++ cocoapodstesttarget/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cocoapodstesttarget
+//
+//  Created by Matthew Hupman on 10/9/13.
+//  Copyright (c) 2013 Gryphn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
